c7576a831aff9c2b442fb5cc83c5c33befb153e3
tripledawg <iamlesliecrouch>
2021-11-19 15:06:59 -0500

    created anchor tag referencing getHighScores, added timer clear to reset,  added styling to buttons, swapped high score table for high score list per demo

--- script.js
+++ script.js
@@ -54,21 +54,40 @@ var questionBank = [
 ];
 ///////////////////////////////////////////////////////////////////////////////
 
+
 // variables to keep track of quiz state
 var timeLeft = 75;
 var timerInterval;
 // var savedScore = 1; //putting in starting numbers to define these vars as integers, not used yet
 var questionBankIndex = -1//so it starts out empty at first
-var viewHighScoreEl = document.getElementById("high-score");
 var highScores = [];
 localStorage.setItem("highscores", highScores);
 // variables to reference start button
 var startButtonEl = document.getElementById("startButton");
 startButtonEl.addEventListener("click", timer);
+// startButtonEl.setAttribute = ("style", "background-color: #9370db; border-radius: 5px; color: white");
 var containerEl = document.getElementById("container");
 var initialsEntered;
 
+
 /////////////////////////////////////////////////////////////////////////////////
+function getStartPage() {
+  while (containerEl.firstChild) {
+    containerEl.removeChild(containerEl.firstChild);//cycles through twice to clear out the container element of the Welcome message and start button once the start button is clicked
+  }
+  var timeEl = document.getElementById("time-left"); 
+  timeEl.textContent= "Time Left: ";
+  timeLeft = 75;
+  startButtonEl = document.createElement("button"); 
+  startButtonEl.addEventListener("click", timer);
+  var welcomeDiv = document.createElement("div"); 
+  welcomeDiv.textContent = "Welcome to the Quiz!";
+  containerEl.appendChild(welcomeDiv);
+  containerEl.appendChild(startButtonEl); 
+  startButtonEl.textContent = "Start!";
+  startButtonEl.setAttribute("style", "background-color: #9370db; border-radius: 5px; color: white"); 
+}
+//////////////////////////
 
 //function that starts the quiz
 function getQuestionsAndAnswers() {
@@ -87,7 +106,7 @@ function getQuestionsAndAnswers() {
   //two step process for creating and populating the ordered list 
   var ulEl = document.createElement("ul");
   containerEl.appendChild(ulEl);
-  ulEl.setAttribute("style", "list-style-type: none");
+  ulEl.setAttribute("style", "list-style-type: none");//gets rid of bullets
   //using index of array for questions and then dot notation to call up that question's answer choices
   //each time the question loads it will programmatically create a list of buttons with textContent set to the answers from the array/object
   questionBank[questionBankIndex].answers.forEach((answer, index) => {//answer is not to be confused with answers from the array/object  array.prototype.forEach
@@ -100,6 +119,7 @@ function getQuestionsAndAnswers() {
     //setting up event listener for each button
     if ((questionBankIndex + 1) === questionBank.length) {
       buttonEl.addEventListener("click", endQuiz);
+
     } else {
       buttonEl.addEventListener("click", getQuestionsAndAnswers);
     }
@@ -172,30 +192,26 @@ function endQuiz() {
   var labelEl = document.createElement("label");
   var inputEl = document.createElement("input");
   var buttonEl = document.createElement("input");
-  ///* <label for="lname">Last name:</label>
-  //<input type="text" id="lname" name="lname">
   containerEl.appendChild(allDone);
   containerEl.appendChild(labelEl);
   containerEl.appendChild(inputEl);
   containerEl.appendChild(buttonEl);
   inputEl.setAttribute("id", "initials")
   labelEl.textContent = "Enter your initials here";
-  // initialsEntered.textContent= initials;
-  //this button needs to be an input/submit button
-  //<input type="button" value="Click me" onclick="msg()"></input>
   buttonEl.setAttribute("value", "Submit");
   buttonEl.setAttribute("type", "button");
   buttonEl.setAttribute("onclick", "getHighScores()");
-  allDone.textContent = "All Done!  Your score is " + userScore + " seconds left."
-  //check if user ran out of time//
+  buttonEl.setAttribute("style", "background-color: #9370db; border-radius: 5px; color: white");
+  allDone.textContent = "All Done!  Your score is " + userScore + " seconds left.";
+verifyAnswer(event); //This makes it look like the example, but it seems confusing to have the previous question feedback showing after you've moved on. 
 
 }
 
 /////////////////////////////////////////////////////////////////
-
-//make sure the value entered isn't empty//
+//make a container to link View High Scores to getHighScores???
+//check if user ran out of time //
 function getHighScores() {
-  // Check that initials element exists and isn't empty
+  // Checks that initials element exists and isn't empty
   if (document.querySelector("#initials")) {
     var initials = document.querySelector("#initials").value;
     var score = { score: localStorage.getItem("userScore"), initials: initials };
@@ -205,45 +221,57 @@ function getHighScores() {
   while (containerEl.firstChild) {
     containerEl.removeChild(containerEl.firstChild);
   }
-  var tableEl = document.createElement("table");
-  containerEl.appendChild(tableEl);
-  var tableHeaderRowEl = document.createElement("tr");
-  var tableHeaderInitialsEl = document.createElement("th");
-  var tableHeaderScoreEl = document.createElement("th");
-  tableEl.appendChild(tableHeaderRowEl);
-  tableHeaderRowEl.appendChild(tableHeaderInitialsEl);
-  tableHeaderRowEl.appendChild(tableHeaderScoreEl);
-  tableHeaderInitialsEl.textContent = "Initials";
-  tableHeaderScoreEl.textContent = "Score";
-  highScores.forEach(element => {
-    var tableRowEl = document.createElement("tr");
-    var tableDataInitials = document.createElement("td");
-    var tableDataScore = document.createElement("td");
-    tableEl.appendChild(tableRowEl);
-    tableRowEl.appendChild(tableDataInitials);
-    tableRowEl.appendChild(tableDataScore);
-    tableDataInitials.textContent = element.initials;
-    tableDataScore.textContent = element.score;
+  // var tableEl = document.createElement("table");
+  // containerEl.appendChild(tableEl);
+  // var tableHeaderRowEl = document.createElement("tr");
+  // var tableHeaderInitialsEl = document.createElement("th");
+  // var tableHeaderScoreEl = document.createElement("th");
+  // tableEl.appendChild(tableHeaderRowEl);
+  // tableHeaderRowEl.appendChild(tableHeaderInitialsEl);
+  // tableHeaderRowEl.appendChild(tableHeaderScoreEl);
+  // tableHeaderInitialsEl.textContent = "Initials";
+  // tableHeaderScoreEl.textContent = "Score";
+  // highScores.forEach(element => {
+  //   var tableRowEl = document.createElement("tr");
+  //   var tableDataInitials = document.createElement("td");
+  //   var tableDataScore = document.createElement("td");
+  //   tableEl.appendChild(tableRowEl);
+  //   tableRowEl.appendChild(tableDataInitials)
+  //   tableRowEl.appendChild(tableDataScore);
+  //   tableDataInitials.textContent = element.initials;
+  //   tableDataScore.textContent = element.score;
+  // });
+  var listEl = document.createElement("ul");
+  containerEl.appendChild(listEl); 
+  highScores.forEach (element => {
+    var liEl = document.createElement("li"); 
+    listEl.appendChild(liEl); 
+    liEl.textContent = element.initials + " - " + element.score;
+    liEl.setAttribute("style", "list-style-type: none; background-color: #d8bfd8")
   });
+
+  
   var goBackButton = document.createElement("button");
   goBackButton.textContent = "Go Back";
+  goBackButton.setAttribute("style", "background-color: #9370db; border-radius: 5px; color: white");
   goBackButton.addEventListener("click", function (event) {
     questionBankIndex = -1;
-    getQuestionsAndAnswers();
+    getStartPage();
+
   });
   containerEl.appendChild(goBackButton);
-  //reset questionBank Index to -1 and call GetQuestions and Answers fucntion
+
   var clearButton = document.createElement("button");
   containerEl.appendChild(clearButton);
   clearButton.textContent = "Clear High Scores";
+  clearButton.setAttribute("style", "background-color: #9370db; border-radius: 5px; color: white");
   clearButton.addEventListener("click", function (event) {
     highScores = [];
     getHighScores();
   });
-  //set high scores array to empty array, call get Highscores function
 };
 
-// create object out of high score and initials then place object in the high scores array then each time we sort the array based on the score
+
 //display first five values of hgihscores array in table
 
 //function to save the high score
@@ -290,7 +318,7 @@ function getHighScores() {
 
 //function for the clock 
 //updates time
-//checks if user ran out of time 
+
 
 
 
